85aa2984c2d62d826d9e67585e7d41f4373a83e5
masaki-azure <masaki.azu.re@outlook.com>
2026-09-21 22:14:49 +0900

    fix: tflintの指摘対応およびtfsecの除外設定を追加

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -3,8 +3,6 @@
 
 # TODO: Key Vault名は環境名だけでは衝突する可能性があるため、実装時にサフィックス付与を検討。
 
-# tfsec:ignore:azure-keyvault-specify-network-acl
-# tfsec:ignore:azure-keyvault-no-purge
 
 resource "azurerm_key_vault" "this" {
   name                = "kv-health-${var.environment}"
@@ -15,12 +13,14 @@ resource "azurerm_key_vault" "this" {
   sku_name = "standard"
 
   rbac_authorization_enabled = true
+  # tfsec:ignore:azure-keyvault-no-purge
   purge_protection_enabled   = var.purge_protection_enabled
   soft_delete_retention_days = 7
 
   # TODO: Container Apps (Consumption) は Egress IP が動的なため、
   # ネットワークレベルの Firewall (Deny) は実質機能しない
   # そのため Allow とし、防御線は Managed Identity による RBAC 認可に一任する
+  # tfsec:ignore:azure-keyvault-specify-network-acl
   network_acls {
     default_action = "Allow"
     bypass         = "AzureServices"
